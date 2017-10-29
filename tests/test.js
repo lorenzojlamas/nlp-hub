cc2a7320edd616dd783433ab05e97d63f291d3c0
Marcelo Felman <felman.marcelo@gmail.com>
2017-10-29 01:09:00 -0300

    renamed to nlp

--- tests/test.js
+++ tests/test.js
@@ -1,27 +1,27 @@
-var n = require('../core/core.js');
+var nlp = require('../core/core.js');
 
 var query = 'quiero cancelar mi taxi';
 
 /*
 // First match: most cost-effective solution
-n.firstMatch(query, function(response) {
+nlp.firstMatch(query, function(response) {
     console.log(`The first detected intent was ${response.intent.name} according to ${response.engine}`);
 });
 */
 
 // Best match: most reliable solution
-n.bestMatch(query, function(response){
+nlp.bestMatch(query, function(response){
     console.log(`The best detected intent was ${response.intent.name} according to ${response.engine}`);    
 });
 
 /*
 // Average match: most conservative solution
-n.average(query, function(response){
+nlp.average(query, function(response){
     console.log(response);
 });
 
 // Regression match: most sophisticated solution
-n.regressionMatch(query, function(response){
+nlp.regressionMatch(query, function(response){
     console.log(response);
 });
 */
