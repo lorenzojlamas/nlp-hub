9ff924e3e3147e8ef697aec8083bf374a4172263
Marcelo Felman <felman.marcelo@gmail.com>
2018-04-12 17:31:33 -0300

    major fix for inMem cache

--- index.js
+++ index.js
@@ -33,6 +33,9 @@ module.exports = {
                 cache.put(cacheId, returnValue);
                 callback(returnValue);
             });
+        }
+        else {
+            callback(cachedValue);
         };
     },
 
