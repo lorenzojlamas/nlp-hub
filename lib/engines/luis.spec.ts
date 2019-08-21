abe3089917df540a61957d9c32c00450a010ccd7
modri <el.asiduo@gmail.com>
2019-08-21 20:47:35 -0300

    refactor: change to request. warn no tests

---
+++ lib/engines/luis.spec.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+
+describe("LuisApp", function () {
+    it('Can be instantiated', function() {
+        sut: LuisApp = new LuisApp();
+        
+    });
+});
--- lib/engines/luis.ts
+++ lib/engines/luis.ts
@@ -1,45 +1,48 @@
-// import * as https from 'https';
-// import { IApp } from '../model/app';
-// export class LuisApp {
+import localVarRequest = require('request');
+import http = require('http');
+import { IApp } from '../model/app';
+export class LuisApp {
 
-//     public async luis(app: IApp, utterance: string): Promise<any> {
-//         return new Promise((resolve, reject) => {
-//             const options = {
-//                 host: app.appHost,
-//                 method: 'GET',
-//                 path: `/luis/v2.0/apps/${app.id}?subscription-key=${app.key}&timezoneOffset=0&verbose=true&q=${encodeURIComponent(utterance)}`,
-//             };
-//             const req = https.request(options, (res: any) => {
-//                 res.on('data', (chunk: any) => {
-//                     const luisResponse = JSON.parse(chunk.toString());
-//                     const intent: any = {
-//                         intent: luisResponse.topScoringIntent.intent,
-//                         score: luisResponse.topScoringIntent.score,
-//                     };
+    public async luis(app: IApp, utterance: string): Promise<any> {
+        return new Promise((resolve, reject) => {
+            const options:  localVarRequest.Options = {
+                method: 'GET',
+                uri: `${app.appHost}/luis/v2.0/apps/${app.id}?subscription-key=${app.key}&timezoneOffset=0&verbose=true&q=${encodeURIComponent(utterance)}`,
+            };
 
-//                     const myResponse: any = {
-//                         engine: 'luis',
-//                         entities: [],
-//                         intent,
-//                         originalResponse: luisResponse,
-//                     };
-//                     luisResponse.entities.forEach((e: any) => {
-//                         myResponse.entities.push({
-//                             score: e.score,
-//                             type: e.type,
-//                             value: e.entity,
-//                         });
-//                     });
-//                     resolve(myResponse);
-//                 });
-//             });
-//             req.on('error', (e: any) => {
-//                 console.log('problem with request: ' + e.message);
-//             });
-//             // write data to request body
-//             req.write('data\n');
-//             req.write('data\n');
-//             req.end();
-//         }) ;
-//     }
-// }
+            new Promise<{ response: http.IncomingMessage; body: any; }>((resolve, reject) => {
+                localVarRequest(options, (error, response, body) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        if (response.statusCode && response.statusCode >= 200 && response.statusCode <= 299) {
+
+                            /* TODO: Tipar */
+                            const intent: any = {
+                                intent: body.topScoringIntent.intent,
+                                score: body.topScoringIntent.score,
+                            };
+                            /* TODO: Hacer un tipo */
+                            const myResponse: any = {
+                                engine: 'luis',
+                                entities: [],
+                                intent,
+                                originalResponse: body,
+                            };
+                            body.entities.forEach((e: any) => {
+                                myResponse.entities.push({
+                                    score: e.score,
+                                    type: e.type,
+                                    value: e.entity,
+                                });
+                            });
+                            resolve({ response: response, body: myResponse });
+                        } else {
+                            reject({ response: response, body: body });
+                        }
+                    }
+                });
+            });
+        }) ;
+    }
+}
